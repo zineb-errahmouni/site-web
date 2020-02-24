39128ad05544c11153e7480f2bed7a94707a518c
zineb <zineb.errahmouni@gmail.com>
2020-02-24 22:16:24 +0100

    diff site-web

---
+++ src/app/views/home/chat/chat-dialog/chat-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {ChatService, Message} from '../chat.service';
+import {Observable} from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-chat-dialog',
+  templateUrl: './chat-dialog.component.html',
+  styleUrls: ['./chat-dialog.component.css']
+})
+export class ChatDialogComponent implements OnInit {
+
+  messages: Observable<Message[]>;
+  val: string;
+
+  constructor(private chat: ChatService) { }
+
+  ngOnInit(): void {
+   this.messages = this.chat.conversation.asObservable()
+     .pipe(
+       scan((acc, val) => acc.concat(val)
+     ));
+  }
+
+  sendMessage() {
+    this.chat.converse(this.val);
+    this.val = '';
+  }
+
+}
---
+++ src/app/views/home/chat/chat.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+
+import {ApiAiClient} from 'api-ai-javascript/es6/ApiAiClient';
+import {BehaviorSubject} from 'rxjs';
+
+
+export class Message {
+  constructor(public content: string, public  sentBy: string) {
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  readonly token = environment.dialogFlow.angularBot;
+  readonly client = new ApiAiClient({accessToken : this.token});
+
+  conversation = new BehaviorSubject<Message[]>([]);
+
+
+
+  talk() {
+    this.client.textRequest('who are you')
+    .then(res => console.log(res));
+  }
+
+  constructor() { }
+
+  updateMessage(message: Message) {
+    this.conversation.next([message]);
+  }
+
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'user');
+    this.updateMessage(userMessage);
+    return this.client.textRequest(msg)
+      .then(res => {
+         const speech = res.result.fulfillment.speech;
+         const botMessage = new Message(speech, 'bot');
+         this.updateMessage(botMessage);
+      });
+
+  }
+
+}
